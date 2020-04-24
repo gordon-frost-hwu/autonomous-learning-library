69e091c321e76f1aeb730933fc97408428df21a4
Gordon Frost <gordon.frost.hwu@googlemail.com>
2020-04-24 20:04:52 +0100

    don't look up min/max values from the environment resource but sample the box object numerous times. A bit brute force-ish but some box properties (velocities for example) can be infinite

--- all/nn/__init__.py
+++ all/nn/__init__.py
@@ -5,62 +5,13 @@ from torch.nn import functional as F
 import numpy as np
 from all.environments import State
 
-class Normalizer(object):
-    def __init__(self):
-        self.initialised = False
-        self.n = None
-        self.mean = None
-        self.mean_diff = None
-        self.var = None
-
-    def initialise(self, inputs):
-        num_inputs = inputs[0].shape
-        device = inputs.device
-        self.n = torch.zeros(num_inputs).to(device)
-        self.mean = torch.zeros(num_inputs).to(device)
-        self.mean_diff = torch.zeros(num_inputs).to(device)
-        self.var = torch.zeros(num_inputs).to(device)
-        self.initialised = True
-
-    def _observe(self, x):
-        self.n += 1.
-        last_mean = self.mean.clone()
-        self.mean += (x-self.mean)/self.n
-        self.mean_diff += (x-last_mean)*(x-self.mean)
-        self.var = torch.clamp(self.mean_diff/self.n, min=1e-2)
-
-    def normalize(self, inputs):
-        if type(inputs) is State:
-            inputs = inputs.raw
-
-        if not self.initialised:
-            self.initialise(inputs)
-
-        result = torch.empty(inputs.shape[0], inputs.shape[1], dtype=torch.float).to("cuda")
-
-        idx = 0
-        for input in inputs:
-            self._observe(input)
-            obs_std = torch.sqrt(self.var)
-            result[idx, :] = (input - self.mean)/obs_std
-        return result
-
-class newNormalizer:
-    def __init__(self, size=0):
-        self.maxValues = np.zeros((1, size))
-        self.minValues = np.zeros((1, size))
-
-    def setMinMaxFromGymEnv(self, box):
-        self.maxValues = torch.tensor(box.high, device="cuda")
-        self.minValues = torch.tensor(box.low, device="cuda")
-
-    def update(self, values):
-        compMax = np.greater(values, self.maxValues)
-        idxMax = np.where(compMax==True)
-        np.put(self.maxValues, idxMax, values[idxMax])
-        compMin = np.less(values, self.minValues)
-        idxMin = np.where(compMin==True)
-        np.put(self.minValues, idxMin, values[idxMin])
+class Normalizer:
+    def __init__(self, box):
+        samples = np.array([box.sample() for x in range(10000)])
+        low = np.min(samples, axis=0)
+        high = np.max(samples, axis=0)
+        self.maxValues = torch.tensor(high, device="cuda")
+        self.minValues = torch.tensor(low, device="cuda")
 
     def normalize(self, values):
         normVal = 2*(values - self.minValues) / (self.maxValues - self.minValues) - 1
@@ -85,8 +36,7 @@ class RLNetwork(nn.Module):
         self.device = next(model.parameters()).device
         self._normalizer = None
         if normalize_inputs is True and box is not None:
-            self._normalizer = newNormalizer()
-            self._normalizer.setMinMaxFromGymEnv(box)
+            self._normalizer = Normalizer(box)
         self.count = 0
 
     def forward(self, state):
@@ -95,7 +45,9 @@ class RLNetwork(nn.Module):
 
             with torch.no_grad():
                 features = self._normalizer.normalize(state.features)
-            # print("features: {0}".format(features))
+
+            # print("original features: {0}".format(state.features))
+            # print("normalised features: {0}".format(features))
 
         else:
             features = state.features.float()
